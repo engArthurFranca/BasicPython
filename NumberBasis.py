1dda1c5b31c567b3d23c008d4341b4d667850107
Arthur França <eng.arthurfranca@hotmail.com>
2022-11-16 21:25:41 -0300

    Set a dealing function

--- NumberBasis.py
+++ NumberBasis.py
@@ -12,3 +12,14 @@ def binary_to_decimal(number: int):
     return sum(
         [ int(elem)*2**n for n, elem in enumerate(list(str(number)))[::-1] ]
         )
+
+def convert_number(number: int, is_decimal:bool=True):
+    if is_decimal:
+        number_binary = decimal_to_binary(number)
+        number_decimal = number
+    else:
+        number_binary = number
+        number_decimal = binary_to_decimal(number)
+    print('')
+    print('Decimal = ', number_decimal)
+    print('Binary = ', number_binary)
