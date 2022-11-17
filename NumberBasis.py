bfee86ce8d621bc781557116760cd007a97db84a
Arthur França <eng.arthurfranca@hotmail.com>
2022-11-16 21:12:06 -0300

    Set converter funcionts

--- NumberBasis.py
+++ NumberBasis.py
@@ -1,6 +1,14 @@
 #Convertion between diferent numerical basis
+
 def decimal_to_binary(number: int):
-    return
+    number_binary = ''
+    while number != 0 and number != 1:
+        number_binary += str(int(number%2))
+        number = int(number/2)
+    number_binary += str(int(number))
+    return int(number_binary[::-1])
 
 def binary_to_decimal(number: int):
-    return
+    return sum(
+        [ int(elem)*2**n for n, elem in enumerate(list(str(number)))[::-1] ]
+        )
